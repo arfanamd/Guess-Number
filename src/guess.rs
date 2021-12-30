95207d3d7fcbfc16c5c1b7326bf5984517a15244
Arfan Ahmad <arfanachmad11@gmail.com>
2021-12-30 23:10:50 +0700

    Implement constructor of guess module.

--- src/guess.rs
+++ src/guess.rs
@@ -1,5 +1,38 @@
-mod guess {
+pub mod guess {
+	const DECK: usize = 52;
+	const PLAY: usize = 21;
+	const ROWS: usize = PLAY / 3;
 
+	pub struct deck {
+		pub arr_nums: [u32; PLAY],
+	}
+	impl deck {
+	// private member functions.
+		fn duplicated(&self, number: u32) -> bool {
+			for element in self.arr_nums {
+				if element == 0 {
+					return false;
+				} else if element == number {
+					return true;
+				}
+			}
+			return false;
+		}
+	// public member functions.
+		pub fn generate() -> deck {
+			let mut stack = deck { arr_nums: [0; PLAY], };
+			let mut INDEX: usize = 0;
+
+			while stack.arr_nums[PLAY - 1] == 0 {
+				let seed: u32 = (rand::random::<u32>() % DECK as u32) + 1;
+				if ! stack.duplicated(seed) {
+					stack.arr_nums[INDEX] = seed;
+					INDEX += 1;
+				}
+			}
+			return stack;
+		}
+	}
 }
 
 // vim: noexpandtab
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#![warn(deprecated_in_future)]
+#![forbid(deprecated)]
+#![allow(non_camel_case_types, non_snake_case)]
+
 mod guess;
+use guess::guess::deck;
 
 fn main() {
-
+	let cards = deck::generate();
+	println!("{:?}", cards.arr_nums);
 }
 
 // vim: noexpandtab
